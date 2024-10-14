d2d9db1c0c8834da6ae1d5a08ee327ebee88cb4a
lostojicc <ostojiclukaftn@gmail.com>
2024-10-15 00:19:33 +0200

    feat: API and Infrastructure
    - created endpoint for http requests
    - injected dependencies
    - tested functionality
    - fixed errors

--- src/Modules/Tours/Explorer.Tours.API/Dtos/KeyPointDto.cs
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/KeyPointDto.cs
@@ -1,12 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace Explorer.Tours.API.Dtos;
-
-public class KeyPointDto {
+﻿public class KeyPointDto {
     public int Id { get; set; }
     public double Latitude { get; set; }
     public double Longitude { get; set; }
     public string Name { get; set; }
     public string? Description { get; set; }
-    public IFormFile Image { get; set; }
+    public string? Image { get; set; } 
 }
--- src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
@@ -3,23 +3,26 @@ using Explorer.Tours.API.Dtos;
 using Explorer.Tours.Core.Domain;
 using Microsoft.AspNetCore.Http;
 
-namespace Explorer.Tours.Core.Mappers;
-
-public class ToursProfile : Profile
-{
-    public ToursProfile()
-    {
+public class ToursProfile : Profile {
+    public ToursProfile() {
         CreateMap<EquipmentDto, Equipment>().ReverseMap();
-        CreateMap<KeyPointDto, KeyPoint>().ForMember(dest => dest.Image, opt => opt.MapFrom(src => ConvertToByteArray(src.Image)));
+
+        CreateMap<KeyPointDto, KeyPoint>()
+            .ForCtorParam("name", opt => opt.MapFrom(src => src.Name))
+            .ForCtorParam("description", opt => opt.MapFrom(src => src.Description))
+            .ForCtorParam("latitude", opt => opt.MapFrom(src => src.Latitude))
+            .ForCtorParam("longitude", opt => opt.MapFrom(src => src.Longitude))
+            .ForCtorParam("image", opt => opt.MapFrom(src => ConvertToByteArray(src.Image)))
+            .ReverseMap()
+            .ForMember(dest => dest.Image, opt => opt.MapFrom(src => ConvertToBase64String(src.Image)));
+
     }
 
-    private byte[] ConvertToByteArray(IFormFile imageFile) {
-        if (imageFile == null) 
-            return null;
+    private byte[] ConvertToByteArray(string base64Image) {
+        return string.IsNullOrEmpty(base64Image) ? null : Convert.FromBase64String(base64Image);
+    }
 
-        using (var memoryStream = new MemoryStream()) {
-            imageFile.CopyTo(memoryStream);
-            return memoryStream.ToArray();
-        }
+    private string ConvertToBase64String(byte[] image) {
+        return image == null ? null : Convert.ToBase64String(image);
     }
 }
