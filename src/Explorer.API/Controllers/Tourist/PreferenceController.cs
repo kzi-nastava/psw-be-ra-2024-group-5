c0c41887a0a480c09f3fde2391a07aedd4922ae0
unknown <nina2dodig@gmail.com>
2024-10-17 01:31:32 +0200

    feat: Added Controller

---
+++ src/Explorer.API/Controllers/Tourist/PreferenceController.cs
@@ -0,0 +1,41 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public.Administration;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Tourist
+{
+    [Authorize(Policy = "touristPolicy")]
+    [Route("api/tourist/preference")]
+    public class PreferenceController : BaseApiController
+    {
+        private readonly IPreferenceService _preferenceService;
+
+        public PreferenceController(IPreferenceService preferenceService)
+        {
+            _preferenceService = preferenceService;
+        }
+
+        [HttpPost]
+        public ActionResult<PreferenceDto> Create([FromBody] PreferenceDto preference)
+        {
+            var result = _preferenceService.Create(preference);
+            return CreateResponse(result);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<PreferenceDto> Update([FromBody] PreferenceDto preference)
+        {
+            var result = _preferenceService.Update(preference);
+            return CreateResponse(result);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var result = _preferenceService.Delete(id);
+            return CreateResponse(result);
+        }
+    }
+}
