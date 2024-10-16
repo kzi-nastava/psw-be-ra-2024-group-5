d6f7c0b63610f5677857e6d339a23842f46fd75c
ra50ivana <ivanaglamocic666@gmail.com>
2024-10-17 00:29:16 +0200

    feat: Added Profile controller and set up dependency injection

---
+++ src/Explorer.API/Controllers/UserProfileController.cs
@@ -0,0 +1,53 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+using Explorer.Tours.API.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers
+{
+    [Route("api/administration/profile")]
+    public class UserProfileController : BaseApiController
+    {
+        private readonly IUserProfileService _userProfileService;
+
+        public UserProfileController(IUserProfileService userProfileService)
+        {
+            _userProfileService = userProfileService;
+        }
+        [HttpPost]
+        public ActionResult<UserProfileDto> Create([FromBody] UserProfileDto userProfile)
+        {
+            var result = _userProfileService.Create(userProfile);
+            return CreateResponse(result);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<UserProfileDto> GetProfile(long id)
+        {
+            var result = _userProfileService.Get(id);
+            return CreateResponse(result);
+        }
+
+        [HttpPut("{id:int}")]
+        [Authorize]
+        public ActionResult<UserProfileDto> UpdateProfile(int id, [FromBody] UserProfileDto userProfile)
+        {
+            var currentUserId = GetCurrentUserId();  
+            if (userProfile.UserId != currentUserId)
+            {
+                return Forbid();  
+            }
+
+            userProfile.Id = id;
+            var result = _userProfileService.Update(userProfile);
+            return CreateResponse(result);
+        }
+
+        private long GetCurrentUserId()
+        {
+            return long.Parse(User.FindFirst("id")?.Value ?? "0");
+        }
+    }
+}
