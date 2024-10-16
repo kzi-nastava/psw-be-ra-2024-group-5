2377db3629d34aa7f3aceb572a790349df644190
Filip Radojicic <filipra2002@gmail.com>
2024-10-16 23:35:44 +0200

    feat/ Implemented service for tour review

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/TourReviewDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public class TourReviewDto
+    {
+        public int Id { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+        public DateTime VisitDate { get; set; }
+        public DateTime ReviewDate { get; set; }
+        public string? Image { get; set; }
+        public int TourId { get; set; }
+        public int TouristId { get; set; }
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/TourReview.cs
@@ -0,0 +1,58 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class TourReview : Entity
+    {
+
+        public int Rating { get; private set; }
+        public string Comment { get; private set; }
+        public DateTime VisitDate { get; private set; }
+        public DateTime ReviewDate { get; private set; }
+        public byte[]? Image { get; private set; }
+        public int TourId { get; private set; }
+        public int TouristId { get; private set; }
+
+        public TourReview(int rating, string comment, DateTime visitDate, int tourId, int touristId, byte[]? image = null)
+        {
+            ValidateRating(rating);
+            ValidateComment(comment);
+            ValidateVisitDate(visitDate);
+
+            Rating = rating;
+            Comment = comment;
+            VisitDate = visitDate;
+            ReviewDate = DateTime.UtcNow;
+            TourId = tourId;
+            TouristId = touristId;
+            Image = image;
+        }
+
+        private void ValidateRating(int rating)
+        {
+            if (rating < 1 || rating > 5)
+            {
+                throw new ArgumentOutOfRangeException(nameof(rating), "Rating must be between 1 and 5.");
+            }
+        }
+
+        private void ValidateComment(string comment)
+        {
+            if (string.IsNullOrWhiteSpace(comment))
+            {
+                throw new ArgumentNullException(nameof(comment), "Comment cannot be null or empty.");
+            }
+        }
+
+        private void ValidateVisitDate(DateTime visitDate)
+        {
+            if (visitDate > DateTime.UtcNow)
+            {
+                throw new ArgumentException("Visit date cannot be in the future.", nameof(visitDate));
+            }
+        }
+
+    }
+}
