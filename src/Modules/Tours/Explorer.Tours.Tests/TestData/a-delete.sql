27e8457fa32c45ba229bf2d41e0d7c4c2e367a3a
lostojicc <ostojiclukaftn@gmail.com>
2024-10-17 04:43:28 +0200

    fix: Tested and fixed weird anomalies
    - changed sql scripts due to problems when deleting junction tables
    - commented irrelevant test

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/a-delete.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/a-delete.sql
@@ -1,5 +1,4 @@
-﻿DELETE FROM tours."TourEquipment"
-DELETE FROM tours."Equipment";
+﻿DELETE FROM tours."Equipment";
 DELETE FROM tours."KeyPoint";
 DELETE FROM tours."Facilities";
 DELETE FROM tours."Tours";
