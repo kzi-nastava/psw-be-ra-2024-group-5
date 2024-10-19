031972d42c7240c21b6eda19490a990a9ac2a1c6
stamenkovicx <natasastamenkovic99@gmail.com>
2024-10-19 14:02:47 +0200

    feat: added tourist repository

--- src/Modules/Tours/Explorer.Tours.Infrastructure/ToursStartup.cs
+++ src/Modules/Tours/Explorer.Tours.Infrastructure/ToursStartup.cs
@@ -2,11 +2,13 @@ using Explorer.BuildingBlocks.Core.UseCases;
 using Explorer.BuildingBlocks.Infrastructure.Database;
 using Explorer.Tours.API.Public.Administration;
 using Explorer.Tours.API.Public.Author;
+using Explorer.Tours.API.Public.Tourist;
 using Explorer.Tours.Core.Domain;
 using Explorer.Tours.Core.Domain.RepositoryInterfaces;
 using Explorer.Tours.Core.Mappers;
 using Explorer.Tours.Core.UseCases.Administration;
 using Explorer.Tours.Core.UseCases.Author;
+using Explorer.Tours.Core.UseCases.Tourist;
 using Explorer.Tours.Infrastructure.Database;
 using Explorer.Tours.Infrastructure.Database.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -31,9 +33,11 @@ public static class ToursStartup
         services.AddScoped<IEquipmentService, EquipmentService>();
         services.AddScoped<IKeyPointService, KeyPointService>();
         services.AddScoped<IFacilityService, FacilityService>();
-    }
+		services.AddScoped<ITouristService, TouristService>();
 
-    private static void SetupInfrastructure(IServiceCollection services)
+	}
+
+	private static void SetupInfrastructure(IServiceCollection services)
     {
         services.AddScoped(typeof(ICrudRepository<Tour>), typeof(CrudDatabaseRepository<Tour, ToursContext>));
         services.AddScoped(typeof(ICrudRepository<Equipment>), typeof(CrudDatabaseRepository<Equipment, ToursContext>));
@@ -41,8 +45,11 @@ public static class ToursStartup
         services.AddScoped<ITourRepository, TourRepository>();
         services.AddScoped(typeof(ICrudRepository<KeyPoint>), typeof(CrudDatabaseRepository<KeyPoint, ToursContext>));
         services.AddScoped(typeof(ICrudRepository<Facility>), typeof(CrudDatabaseRepository<Facility, ToursContext>));
+		services.AddScoped<ITouristRepository, TouristRepository>();
 
-        services.AddDbContext<ToursContext>(opt =>
+
+
+		services.AddDbContext<ToursContext>(opt =>
             opt.UseNpgsql(DbConnectionStringBuilder.Build("tours"),
                 x => x.MigrationsHistoryTable("__EFMigrationsHistory", "tours")));
     }
