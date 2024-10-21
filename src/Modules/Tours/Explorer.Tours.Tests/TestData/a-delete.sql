96d2e611be2440f810de2286edb977ad792bdf92
HakTak <katek.m536@gmail.com>
2024-10-21 18:40:18 +0200

    feat: small fixes

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/a-delete.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/a-delete.sql
@@ -3,5 +3,5 @@
 DELETE FROM tours."Equipment";
 DELETE FROM tours."KeyPoint";
 DELETE FROM tours."Facilities";
-DELETE FROM tours."Tours";
 DELETE FROM tours."Preferences";
+DELETE FROM tours."Tours";
