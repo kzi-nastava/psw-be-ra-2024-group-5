1cdbc46bdc3edb8f0916c23e86924a0e43ae4905
Nikola Tesic <nikolatesic202@gmail.com>
2024-10-24 01:58:00 +0200

    fix: Removed redundant function

--- src/Explorer.API/Controllers/Tourist/ClubController.cs
+++ src/Explorer.API/Controllers/Tourist/ClubController.cs
@@ -5,7 +5,6 @@ using Explorer.Tours.API.Dtos;
 using Explorer.Tours.API.Public.Administration;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
 
 namespace Explorer.API.Controllers.Tourist
 {
@@ -30,17 +29,6 @@ namespace Explorer.API.Controllers.Tourist
             [HttpPost]
             public ActionResult<ClubDto> Create([FromBody] ClubDto club)
             {
-                var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == "id");
-                if (userIdClaim == null)
-                {
-                    club.OwnerId = 0;
-                }
-                else
-                {
-                    var userId = long.Parse(userIdClaim.Value);
-                    club.OwnerId = userId;
-                }
-
             var result = _clubService.Create(club);
                 return CreateResponse(result);
             }
