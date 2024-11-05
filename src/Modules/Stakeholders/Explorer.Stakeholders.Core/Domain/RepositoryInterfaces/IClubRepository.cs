5e915fb2c7d9fbf919908bcfd704d1cc14bce6dc
belegisanin <belegisanind@gmail.com>
2024-11-05 20:52:23 +0100

    feat: Repository for aggregate

--- src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/RepositoryInterfaces/IClubRepository.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/RepositoryInterfaces/IClubRepository.cs
@@ -1,15 +1,14 @@
 ﻿using Explorer.BuildingBlocks.Core.UseCases;
-using FluentResults;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Explorer.Stakeholders.Core.Domain.RepositoryInterfaces
 {
-    public interface IClubRepository : ICrudRepository<Club>
+    public interface IClubRepository
     {
+        Club? Get(long clubId);
+        PagedResult<Club> GetPaged(int page, int pageSize);
+        Club Create(Club club);
+        Club Update(Club club);
+        void Delete(long clubId);
         bool DeleteMembership(long clubId, long userId);
         ClubMembership? CreateMembership(long clubId, long userId);
         List<ClubMembership> GetAllMemberships();
