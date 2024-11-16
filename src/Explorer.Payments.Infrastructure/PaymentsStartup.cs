e7bd5f5182ba11a7d90c789a8925825aef6fd0e1
Ognjen Adzic <adzic.ra228.2021@uns.ac.rs>
2024-11-16 17:45:44 +0100

    Feat: added new module for payments

---
+++ src/Explorer.Payments.Infrastructure/PaymentsStartup.cs
@@ -0,0 +1,31 @@
+﻿using Explorer.Payments.Core.Mappers;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Payments.Infrastructure
+{
+    public static class PaymentsStartup
+    {
+        public static IServiceCollection ConfigurePaymentsModule(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(PaymentsProfile).Assembly);
+            SetupCore(services);
+            SetupInfrastructure(services);
+            return services;
+        }
+
+        private static void SetupCore(IServiceCollection services)
+        {
+
+        }
+
+        private static void SetupInfrastructure(IServiceCollection services)
+        {
+
+        }
+        }
+}
