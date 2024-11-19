ec2790752b023a03c65114bde76d2f3ccf5ac3b6
nkusla <nikola.kuslakovic1616@gmail.com>
2024-11-19 11:07:38 +0100

    feat: Implemented generic ICrudService interface
    - Extended Delete method in CrudDatabaseRepository to catch KeyNotFound errors
    - CrudService now extends ICrudService

---
+++ src/BuildingBlocks/Explorer.BuildingBlocks.Core/UseCases/ICrudService.cs
@@ -0,0 +1,16 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.BuildingBlocks.Core.UseCases;
+public interface ICrudService<TDto, TDomain> where TDomain : Entity {
+    public Result<PagedResult<TDto>> GetPaged(int page, int pageSize);
+    public Result<TDto> Get(int id);
+    public Result<TDto> Create(TDto entity);
+    public Result<TDto> Update(TDto entity);
+    public Result Delete(int id);
+}
