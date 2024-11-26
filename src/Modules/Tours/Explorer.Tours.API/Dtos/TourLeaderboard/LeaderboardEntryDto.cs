b731388a0b8c6c38595369140c99de037c822550
belegisanin <belegisanind@gmail.com>
2024-11-26 20:16:36 +0100

    feat: Leaderboards added
    - Getting the leaderboard from tour service
    - Model changes to TourExecution to include start time
    - TourLeaderboard DTO
    - Other DTOs
    - Repository changes for needed data
    - Controller method for getting the leaderboard

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/TourLeaderboard/LeaderboardEntryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Explorer.Tours.API.Dtos.TourLeaderboard;
+
+public class LeaderboardEntryDto
+{
+    public int Position { get; set; }
+    public long UserId { get; set; }
+    public string UserName { get; set; }
+    public string? ProfileImage { get; set; }
+    public TimeSpan Time { get; set; }
+}
