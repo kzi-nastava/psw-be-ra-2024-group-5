4f3012ac530f84bd7bdf0e3893a660f8a0556d51
lostojicc <ostojiclukaftn@gmail.com>
2024-11-28 00:28:28 +0100

    feat: Hidden location encounter
    - hidden location encounter inherits encounter
    - new dtos for communicating with frontend
    - progressing through and completing hidden location encounters

---
+++ src/Modules/Encounters/Explorer.Encounters.Core/Domain/HiddenLocationEncounter.cs
@@ -0,0 +1,19 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Encounters.Core.Domain {
+    public class HiddenLocationEncounter : Encounter {
+        public Location ImageLocation { get; set; }
+        public byte[] Image { get; set; }
+
+        public HiddenLocationEncounter() { }
+
+        public bool IsCloseToImageLocation(Location location) {
+            return GeoCalculator.IsClose(location, ImageLocation, 5);
+        }
+    }
+}
