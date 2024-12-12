02cca84362f30540e7e396e8ae3ae86d2ad51286
belegisanin <belegisanind@gmail.com>
2024-12-12 11:46:20 +0100

    fix: Tests fix

--- src/Modules/Payments/Explorer.Payments.Tests/TestData/a-init.sql
+++ src/Modules/Payments/Explorer.Payments.Tests/TestData/a-init.sql
@@ -30,7 +30,7 @@ VALUES
     (-22, -22, '', 'Second Tourist Biography', 'Adventure awaits'),
     (-23, -23, '', 'Third Tourist Biography', 'Journey of a lifetime');
 
-INSERT INTO tours."Tours" ("Id", "Name", "Description", "Tags", "Level", "Status", "Price", "AuthorId", "Length", "TransportDurations", "PublishedTime", "ArchivedTime")
+INSERT INTO tours."Tours" ("Id", "Name", "Description", "Tags", "Level", "Status", "Price", "AuthorId", "Length", "TransportDurations", "PublishedTime", "ArchivedTime", "NumberOfViews")
 VALUES 
     (-1, 'Tura1', 'Opis prve ture', 'tag1, tag2', 0, 1, 
         jsonb_build_object('Amount', 20, 'Currency', 0), 
@@ -40,7 +40,7 @@ VALUES
             jsonb_build_object('Transport', 1, 'Duration', 50.5)
         ), 
         '2024-08-22 09:15:00+02', 
-        '2024-12-22 09:15:00+02'),
+        '2024-12-22 09:15:00+02', 10),
 
     (-2, 'Tura2', 'Opis druge ture', 'tag1', 1, 1, 
         jsonb_build_object('Amount', 400, 'Currency', 1), 
@@ -50,7 +50,7 @@ VALUES
             jsonb_build_object('Transport', 1, 'Duration', 230.5)
         ), 
         '2024-06-12 11:22:00+02', 
-        '2024-12-23 08:32:00+02'),
+        '2024-12-23 08:32:00+02', 10),
 
     (-3, 'Tura3', 'Opis treće ture', 'tag1, tag2, tag3', 0, 0,
         jsonb_build_object('Amount', 0, 'Currency', 0), 
@@ -61,4 +61,4 @@ VALUES
             jsonb_build_object('Duration', 70.0, 'Transport', 2)
         ), 
         '2023-02-12 08:21:00+02', 
-        '2024-03-23 08:32:00+02');
+        '2024-03-23 08:32:00+02', 10);
--- src/Modules/Payments/Explorer.Payments.Tests/TestData/b-shopping-carts.sql
+++ src/Modules/Payments/Explorer.Payments.Tests/TestData/b-shopping-carts.sql
@@ -2,6 +2,11 @@ INSERT INTO payments."ShoppingCarts" ("Id", "TotalPrice", "TouristId")
 VALUES 
     (-1, jsonb_build_object('Amount', 20, 'Currency', 0), -21);
 
-INSERT INTO payments."OrderItems" ("Id", "TourId", "TourName", "Price", "ShoppingCartId")
-VALUES 
-    (-1, -1, 'Tura1', jsonb_build_object('Amount', 20, 'Currency', 0), -1);
+--INSERT INTO payments."OrderItems" ("Id", "TourId", "TourName", "Price", "ShoppingCartId")
+--VALUES 
+--    (-1, -1, 'Tura1', jsonb_build_object('Amount', 20, 'Currency', 0), -1);
+
+
+INSERT INTO payments."OrderItems"(
+	"Id", "ItemId", "ItemName", "Price", "IsBundle", "ShoppingCartId")
+	VALUES (-1, -1, 'Tura1', jsonb_build_object('Amount', 20, 'Currency', 0), false, -1);
